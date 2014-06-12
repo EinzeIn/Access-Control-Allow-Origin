b4543a5acabcfb4104514b9f9992dfba83b00857
EinzeIn <einzeln@live.com>
2014-06-12 16:27:00 +0900

    Update background.js

--- background.js
+++ background.js
@@ -1,7 +1,6 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.executeScript({
 	/*Isolate Video Player*/
-	$('body').replaceWith($('object .application/x-shockwave-flash', 'html').innerHTML;
-	//$('<div><a>hoopla</a></div>').appendTo('#Body');
+	$("body").html($("body").html().replaceWith(document.getElementByID('flashcontent').innerHtml));
   });
 });
