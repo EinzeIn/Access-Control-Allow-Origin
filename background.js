6d917057e20b3fe80a6fa672defe5618f918548e
EinzeIn <einzeln@live.com>
2014-06-12 16:34:32 +0900

    Update background.js

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.executeScript({
 	/*Isolate Video Player*/
-	$("body").html($("body").html().replaceWith(document.getElementByID('flashcontent').innerHtml));
+	$("body").html().replaceWith(document.getElementById('flashcontent').innerHtml);
   });
 });
