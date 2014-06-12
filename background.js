a556c664e0404962ace281a263287c0480b1028e
EinzeIn <einzeln@live.com>
2014-06-12 10:48:45 +0900

    Update background.js

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
 chrome.browserAction.onClicked.addListener(function(tab){
 	/*Isolate Video Player*/
-	$('form').replaceWith($('div .application/x-shockwave-flash', 'form').html());
+	$('body').replaceWith($('div .application/x-shockwave-flash', 'body').html());
 });
